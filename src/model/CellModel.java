e5cc074da864c406f4330f4a6954e531b0f0ae4b
Ethan Roe <ethan.h.roe@gmail.com>
2024-09-11 00:44:13 -0500

    Added CellModel class

---
+++ src/model/CellModel.java
@@ -0,0 +1,19 @@
+package model;
+
+public class CellModel {
+	
+	int state;
+	
+	public CellModel(int state) {
+		this.state = state;
+	}
+	
+	public int getState() {
+		return state;
+	}
+	
+	public void setState(int state) {
+		this.state = state;
+	}
+
+}
